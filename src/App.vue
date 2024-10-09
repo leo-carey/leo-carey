6d7ac8691961f1c45a8fecd15f3b76477bc53950
Leonardo Carey Machado <leonardo.machado@PC-000518.local>
2024-10-09 17:28:14 -0300

    chore: formatter and eslint

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,6 @@
 <template>
-  <main class="w-screen h-screen flex items-center justify-center bg-primary-100">
-    <h1 class="text-9xl font-semibold uppercase text-center font-oswald text-secondary-400">
+  <main class="flex h-screen w-screen items-center justify-center bg-primary-100">
+    <h1 class="text-center font-oswald text-9xl font-semibold uppercase text-secondary-400">
       Hello World!
     </h1>
   </main>
